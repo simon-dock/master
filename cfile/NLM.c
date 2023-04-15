8405f0f0431ce249bf09ed93bc84b8bcd3742a8f
simon-dock <simck11769@gmail.com>
2023-04-15 18:28:39 +0900

    rerefactoring

--- cfile/NLM.c
+++ cfile/NLM.c
@@ -1,217 +1,228 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<stdbool.h>
 
 #define H 0.00000000001
+#define STEP 0.5
 #define LIMIT 100000
+#define SIZE 5
+#define NDIMENSION 4
+#define LDIMENSION 2
+#define PROGRESS false
 
-void enter(double **data, int dimension, int size);
-void enter_randam(double **data, int demnsion, int size);
-void assignment(double **data1, double **data2, int demnsion, int size);
-void print_array(double **data, int demension, int size);
-double cal_error(double **BaseData, double **ModifiedData, int basedemen, int modifieddemen, int size);
-double cal_distance(double *data1, double *data2, int dimension);
-void autodifferen(double *partialdifferen, double **BaseData, double **ModifiedData, int basedemen, int modifieddemen, int size);
-double differential(double **BaseData, double **ModifiedData, double **DeltaData, int basedemen, int modifieddemen, int size);
-void extreme(double **BaseData, double **ModifiedData, int basedemen, int modifieddemen, int size);
+double **makeArray(double **data, int dimension, int size);
+void input(double **data, int dimension, int size);
+void inputRandam(double **data, int dimension, int size);
+void assignment(double **data1, double **data2, int dimension, int size);
+void printArray(double **data, int dimension, int size);
+
+double extreme(double **dataOrigin, double **dataLearn, int dimenOrigin, int dimenLearn, int size);
+void autodifferen(double *partdiff, double **dataOrigin, double **dataLearn, int dimenOrigin, int dimenLearn, int size);
+double differential(double **dataOrigin, double **dataLearn, double **dataDelta, int dimenOrigin, int dimenLearn, int size);
+double calError(double **dataOrigin, double **dataLearn, int dimenOrigin, int dimenLearn, int size);
+double calDistance(double *data1, double *data2, int dimension);
+
 
 int main(void){
 
     int i,j;
-    int size = 5;
-    int basedemen = 4;
-    int modifieddemen = 2;
-    double **BaseData;
-    double **ModifiedData;
-
+    int size = SIZE, dimenOrigin = NDIMENSION, dimenLearn = LDIMENSION;
+    double **dataOrigin, **dataLearn;
     double error;
 
-    BaseData = (double**)malloc(sizeof(double*)*size);
-    BaseData[0] = (double*)malloc(sizeof(double)*basedemen*size);
-    if(BaseData == NULL) exit(0);
-    for(i=1; i<size; i++){
-        BaseData[i] = BaseData[i-1] + basedemen;
-    }
+    dataOrigin = makeArray(dataOrigin, dimenOrigin, size);
+    dataLearn = makeArray(dataLearn, dimenLearn, size);
 
-    ModifiedData = (double**)malloc(sizeof(double*)*size);
-    ModifiedData[0] = (double*)malloc(sizeof(double)*modifieddemen*size);
-    if(ModifiedData == NULL) exit(0);
-    for(i=1; i<size; i++){
-        ModifiedData[i] = ModifiedData[i-1] + modifieddemen;
-    }
+    input(dataOrigin, dimenOrigin, size);
+    inputRandam(dataLearn, dimenLearn, size);
 
-    enter(BaseData, basedemen, size);
-    enter_randam(ModifiedData, modifieddemen, size);
+    printArray(dataOrigin, dimenOrigin, size);
+    printArray(dataLearn, dimenLearn, size);
 
-    print_array(BaseData, basedemen, size);
-    print_array(ModifiedData, modifieddemen, size);
+    error = extreme(dataOrigin, dataLearn, dimenOrigin, dimenLearn, size);
 
-    extreme(BaseData, ModifiedData, basedemen, modifieddemen, size);
+    printf("error : %.32f\n", error);
+    printArray(dataLearn, dimenLearn, size);
 
-    free(BaseData);
-    free(ModifiedData);
+    free(dataOrigin);
+    free(dataLearn);
     
-
     return 0;
 }
 
+/*
+配列 array[size][dimension] を動的に確保し先頭アドレスを返す
+メモリの解放忘れに注意 malloc関数によって引数のアドレスからアドレスが変更される
+*/
+double **makeArray(double **data, int dimension, int size){
 
-void enter(double **data, int demnsion, int size){
+    int i;
+
+    data = (double**)malloc(sizeof(double*)*size);
+    data[0] = (double*)malloc(sizeof(double)*dimension*size);
+    if(data == NULL) exit(0);
+    for(i=1; i<size; i++){
+        data[i] = data[i-1] + dimension;
+    }
+
+    return data;
+}
+
+/*
+配列に値を代入
+*/
+void input(double **data, int dimension, int size){
     
     int i,j;
 
     for(i=0;i<size;i++){
-        for(j=0;j<demnsion;j++){
+        for(j=0;j<dimension;j++){
             data[i][j]=i*2;
         }
     }
 }
 
-void enter_randam(double **data, int demnsion, int size){
+/*
+配列に値をランダムに代入
+*/
+void inputRandam(double **data, int dimension, int size){
     
     int i,j;
 
     for(i=0;i<size;i++){
-        for(j=0;j<demnsion;j++){
+        for(j=0;j<dimension;j++){
             data[i][j]=rand()%10;
         }
     }
 }
 
-void assignment(double **data1, double **data2, int demnsion, int size){
+/*
+data1にdata2の値を代入
+二つのdataは配列の形が全く同じ必要がある
+*/
+void assignment(double **data1, double **data2, int dimension, int size){
     
     int i,j;
 
     for(i=0;i<size;i++){
-        for(j=0;j<demnsion;j++){
+        for(j=0;j<dimension;j++){
             data1[i][j]=data2[i][j];
         }
     }
 }
 
-
-
-void print_array(double **data, int demension, int size){
+/*
+配列の中身を表示
+*/
+void printArray(double **data, int dimension, int size){
     
     int i, j;
     
     for(i=0;i<size;i++){
-        for(j=0;j<demension;j++){
-            printf("[%d,%d] = %f\n",i, j, data[i][j]);
+        for(j=0;j<dimension;j++){
+            printf("%f,",data[i][j]);
         }
+        printf("\n");
     }
     printf("\n");
 }
 
+/*
+最急降下法を実行し最終的なEを返す
+*/
+double extreme(double **dataOrigin, double **dataLearn, int dimenOrigin, int dimenLearn, int size){
 
-double cal_error(double **BaseData, double **ModifiedData, int basedemen, int modifieddemen, int size){
+    int i,j;
+    int count = 0;
+    double a = STEP, error = 10000;
+    double *partdiff;
+
+    partdiff = (double*)malloc(sizeof(double)*size*dimenLearn);
+    if(partdiff == NULL) exit(0);
+
+    while(count < LIMIT){
+        count++;
+        autodifferen(partdiff, dataOrigin, dataLearn, dimenOrigin, dimenLearn, size);
+        for(i=0; i<size; i++){
+            for(j=0; j<dimenLearn; j++){
+                dataLearn[i][j] -= a * partdiff[i*dimenLearn+j];
+            }
+        }
+        error = fabs(calError(dataOrigin, dataLearn, dimenOrigin, dimenLearn, size));
+
+        if(PROGRESS == true) printf("%d:%f\n", count, error);
+    }
+
+    free(partdiff);
+
+    return error;
+}
+
+/*
+自動偏微分を実行
+*/
+void autodifferen(double *partdiff, double **dataOrigin, double **dataLearn, int dimenOrigin, int dimenLearn, int size){
+
+    int i,j;
+    double **dataDelta;
+
+    dataDelta = makeArray(dataDelta, dimenLearn, size);
+    assignment(dataDelta, dataLearn, dimenLearn, size);
+
+    for(i=0; i<size; i++){
+        for(j=0; j<dimenLearn; j++){
+            dataDelta[i][j] = dataLearn[i][j] + H;
+            partdiff[i*dimenLearn+j] = differential(dataOrigin, dataLearn, dataDelta, dimenOrigin, dimenLearn, size);
+            dataDelta[i][j] = dataLearn[i][j];
+        }
+    }
+    
+    free(dataDelta);
+}
+
+/*
+微分を実行し値を返す
+*/
+double differential(double **dataOrigin, double **dataLearn, double **dataDelta, int dimenOrigin, int dimenLearn, int size){
+    
+    return (calError(dataOrigin, dataDelta, dimenOrigin, dimenLearn, size) - calError(dataOrigin, dataLearn, dimenOrigin, dimenLearn, size)) / H;
+}
+
+/*
+Eを計算し値を返す
+*/
+double calError(double **dataOrigin, double **dataLearn, int dimenOrigin, int dimenLearn, int size){
     
     int i, j;
-    double error;
     double dist_base, dist_modified;
-    double sum_base = 0.0;
-    double sum_modified = 0.0;
+    double sum_base = 0.0, sum_modified = 0.0;
 
     for(i=0; i<size; i++){
         for(j=i+1; j<size; j++){
-            dist_base = cal_distance(BaseData[i], BaseData[j], basedemen);
-            dist_modified = cal_distance(ModifiedData[i], ModifiedData[j], modifieddemen);
+            dist_base = calDistance(dataOrigin[i], dataOrigin[j], dimenOrigin);
+            dist_modified = calDistance(dataLearn[i], dataLearn[j], dimenLearn);
             sum_base += dist_base;
             sum_modified += pow(dist_base-dist_modified,2.0) / dist_base;
         }
     }
 
-    error = sum_base / sum_modified;
-
-    return error;
+    return sum_base / sum_modified;
 }
 
-
-double cal_distance(double *data1, double *data2, int demension){
+/*
+２点間の距離を計算し値を返す　多次元対応
+*/
+double calDistance(double *data1, double *data2, int dimension){
 
     int i;
-    double difference = 0.0;
-    double sum = 0.0;
-    double distance = 0.0;
+    double difference = 0.0, sum = 0.0;
 
-    for(i=0; i<demension; i++){
+    for(i=0; i<dimension; i++){
         difference = data1[i];
         difference -= data2[i];
         sum += pow(difference,2.0);
     }
 
-    distance = sqrt(sum);
-
-    return distance;
-
-}
-
-void extreme(double **BaseData, double **ModifiedData, int basedemen, int modifieddemen, int size){
-
-    int i,j;
-    double *partialdifferen;
-
-    partialdifferen = (double*)malloc(sizeof(double)*size*modifieddemen);
-    if(partialdifferen == NULL) exit(0);
-
-    double a = 0.5;
-    double error = 10000;
-    int count = 0;
-    int count_partial = 0;
-
-    while(count < LIMIT){
-        count++;
-        autodifferen(partialdifferen, BaseData, ModifiedData, basedemen, modifieddemen, size);
-        count_partial = 0;
-        for(i=0; i<size; i++){
-            for(j=0; j<modifieddemen; j++){
-                ModifiedData[i][j] -= a * partialdifferen[count_partial];
-                count_partial++;
-            }
-        }
-        error = fabs(cal_error(BaseData, ModifiedData, basedemen, modifieddemen, size));
-        //printf("%d:%.32f\n", count, error);
-    }
-
-    print_array(ModifiedData, modifieddemen, size);
-
-    free(partialdifferen);
-}
-
-
-void autodifferen(double *partialdifferen, double **BaseData, double **ModifiedData, int basedemen, int modifieddemen, int size){
-
-    int i,j;
-    int count_partial = 0;
-    double **DeltaData;
-
-    DeltaData = (double**)malloc(sizeof(double*)*size);
-    DeltaData[0] = (double*)malloc(sizeof(double)*modifieddemen*size);
-    if(DeltaData == NULL) exit(0);
-    for(i=1; i<size; i++){
-        DeltaData[i] = DeltaData[i-1] + modifieddemen;
-    }
-    //print_array(DeltaData, modifieddemen, size);
-    assignment(DeltaData, ModifiedData, modifieddemen, size);
-
-    for(i=0; i<size; i++){
-        for(j=0; j<modifieddemen; j++){
-            DeltaData[i][j] = ModifiedData[i][j] + H;
-            partialdifferen[count_partial] = differential(BaseData, ModifiedData, DeltaData, basedemen, modifieddemen, size);
-            DeltaData[i][j] = ModifiedData[i][j];
-            count_partial ++;
-        }
-    }
-
-    free(DeltaData);
-}
-
-
-double differential(double **BaseData, double **ModifiedData, double **DeltaData, int basedemen, int modifieddemen, int size){
-    
-    double fx;
-
-    fx = (cal_error(BaseData, DeltaData, basedemen, modifieddemen, size) - cal_error(BaseData, ModifiedData, basedemen, modifieddemen, size)) / H;
-
-    return fx;
+    return sqrt(sum);
 }
